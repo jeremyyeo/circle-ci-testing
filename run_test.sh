39bbc4d2a7600481188153b7dee2ee6767e6ff8a
jeremyyeo <jeremyyeo@gmail.com>
2023-04-18 16:49:20 +1200

    add tests

---
+++ run_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eo pipefail
+
+cd integration_tests
+
+if [[ ! -e ~/.dbt/profiles.yml ]]; then
+    echo "Copying sample profile"
+    mkdir -p ~/.dbt
+    cp ci/sample.profiles.yml ~/.dbt/profiles.yml
+fi
+
+echo "Starting integration tests"
+dbt debug --target $1
